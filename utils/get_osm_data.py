591f7e814af3e6192beeaf5adde9132b1f1fc2f4
filippodll <filippodalla42@gmail.com>
2025-01-27 16:08:30 +0100

    funziona tutto

--- utils/get_osm_data.py
+++ utils/get_osm_data.py
@@ -118,35 +118,6 @@ if __name__ == "__main__":
 
 
 
-   # # # # #       ELIMINATES OSMID STRINGS         # # # # # 
-
-
-
-
-
-    # for index, row in gdf_nodes.iterrows():
-    # # if "osmid_original" is a list, keep the first element
-    #     old_list = ast.literal_eval(str(row["osmid_original"]))
-    #     if isinstance(old_list, list):
-    #         new_id = old_list[0]
-    #         # update the edges with u_original or v_original in old_list, with new_id
-    #         gdf_edges.loc[gdf_edges["u_original"].isin(old_list), "u_original"] = new_id
-    #         gdf_edges.loc[gdf_edges["v_original"].isin(old_list), "v_original"] = new_id
-    #         # update the node with new_id
-    #         gdf_nodes.loc[index, "osmid_original"] = new_id
-
-    # assert set(gdf_edges["u_original"].astype(str)).issubset(set(gdf_nodes["osmid_original"].astype(str)))
-    # assert set(gdf_edges["v_original"]).issubset(set(gdf_nodes["osmid_original"]))
-
-
-
-
-
-
-
-
-
-
     if parser.allow_duplicates:
         N_DUPLICATES = 0
     else:
@@ -171,20 +142,10 @@ if __name__ == "__main__":
         gdf_edges = gdf_edges.drop_duplicates(subset=["u", "v"])
 
 
-
-    # gdf_edges = gdf_edges[
-    #     ["u", "v", "length", "oneway", "lanes", "highway", "maxspeed", "name"]
-    # ]
-    # gdf_nodes = gdf_nodes[["osmid", "x", "y", "highway"]]
-
-
-
     gdf_edges.to_csv("edges_complete.csv", sep=";", index=False)
     gdf_nodes.to_csv("nodes_complete.csv", sep=";", index=False)
 
     
-
-
 
 
 
@@ -244,16 +205,6 @@ if __name__ == "__main__":
 
 
 
-
-
-    # #Prepare node dataframe
-    # gdf_nodes = gdf_nodes[["osmid", "x", "y", "highway"]]
-    # #Prepare edge dataframe
-
-
-    # gdf_edges = gdf_edges[
-    #     ["u", "v", "length", "oneway", "lanes", "highway", "maxspeed", "name"]
-    # ]
     if parser.allow_duplicates:
         N_DUPLICATES = 0
     else:
